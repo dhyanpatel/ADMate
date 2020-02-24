b5e373edbfeab41815d8dbfd91d2a09348b02d3f
Dhyan Patel <dpate254@uic.edu>
2020-02-24 14:29:36 -0600

    Function for getting users in a group

--- ADMate/AdMate.cs
+++ ADMate/AdMate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.DirectoryServices.AccountManagement;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace ADMate {
@@ -49,15 +50,24 @@ namespace ADMate {
             }
         }
 
+        public List<string> GetGroupUsers(string groupName) {
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(_pc, groupName);
+
+            List<string> result = new List<string>();
+            foreach (var member in group.GetMembers()) {
+                result.Add(member.Name);
+            }
+            return result;
+        }
+
         public List<string> GetUserGroups(string userId) {
             try {
                 UserPrincipal user = UserPrincipal.FindByIdentity(_pc, userId);
 
-                PrincipalSearchResult<Principal> searchResult = user.GetGroups();
 
                 List<string> result = new List<string>();
-                foreach (var found in searchResult) {
-                    result.Add(found.Name);
+                foreach (var group in user.GetGroups()) {
+                    result.Add(group.Name);
                 }
 
                 return result;
